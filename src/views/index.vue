c131ebf9db28ef53ec86b65d0c79df3ac598c29e
吴佳杰 <wujiajie@zhuojianchina.com>
2018-11-08 13:30:15 +0800

    index删除不必要的东西

--- src/views/index.vue
+++ src/views/index.vue
@@ -24,7 +24,7 @@
             <el-menu-item index="/more" id="buttom" class="menu-item">
               <span>更多</span>
             </el-menu-item>
-            <el-submenu index="/x" style="float:right;" class="menu-item">
+            <el-submenu index="" style="float:right;" class="menu-item">
               <template slot="title">我的工作台</template>
               <el-menu-item index="/2-1">选项1</el-menu-item>
               <el-menu-item index="/2-2">选项2</el-menu-item>

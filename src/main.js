536d2ac65ff9ed9b8ca82bdd79265bbbb71317fc
吴佳杰 <wujiajie@zhuojianchina.com>
2018-11-19 13:31:45 +0800

    登陆超时返回首页

--- src/main.js
+++ src/main.js
@@ -11,10 +11,71 @@ import { changeTitle } from '@/utils'
 import contentmenu from 'v-contextmenu'
 import 'v-contextmenu/dist/index.css'
 import uploader from 'vue-simple-uploader'
-import store from '@/store'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
 Vue.use(contentmenu)
 Vue.use(uploader)
 Vue.config.productionTip = false
+
+
+const ADD_TOKEN = 'ADD_TOKEN';
+const REMOVE_TOKEN = 'REMOVE_TOKEN';
+let store = new Vuex.Store({
+  state: {
+    token: '',
+    userId: ''
+  },
+  mutations: {
+    [ADD_TOKEN](state, token) {
+      sessionStorage.setItem("token", token);
+      state.token = token;
+    },
+    [REMOVE_TOKEN](state, token) {
+      sessionStorage.removeItem("token");
+      state.token = token;
+    }
+  },
+  actions: {
+
+  }
+})
+
+//http request拦截器
+axios.interceptors.request.use(
+  config => {
+    var token = sessionStorage.getItem('token');
+    if (token) {
+      // 判断是否存在token，如果存在的话，则每个http header都加上token
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+)
+
+//http response拦截器
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // 返回 401 清除token信息并跳转到登录页面
+          store.commit('REMOVE_TOKEN');
+          router.replace({
+            path: '/',
+            query: { redirect: router.currentRoute.fullPath }
+          })
+      }
+    }
+    return Promise.reject(error.response.data)   // 返回接口返回的错误信息
+  });
 
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
-const ADD_TOKEN = 'ADD_TOKEN';
-const REMOVE_TOKEN = 'REMOVE_TOKEN';
-const store = new Vuex.Store({
-  state: {
-    token: '',
-    userId: ''
-  },
-  mutations: {
-    [ADD_TOKEN](state, token) {
-      sessionStorage.setItem("token", token);
-      state.token = token;
-    },
-    [REMOVE_TOKEN](state, token) {
-      sessionStorage.removeItem("token", token);
-      state.token = token;
-    }
-  },
-  actions: {
+// const ADD_TOKEN = 'ADD_TOKEN';
+// const REMOVE_TOKEN = 'REMOVE_TOKEN';
+// const store = new Vuex.Store({
+//   state: {
+//     token: '',
+//     userId: ''
+//   },
+//   mutations: {
+//     [ADD_TOKEN](state, token) {
+//       sessionStorage.setItem("token", token);
+//       state.token = token;
+//     },
+//     [REMOVE_TOKEN](state, token) {
+//       sessionStorage.removeItem("token", token);
+//       state.token = token;
+//     }
+//   },
+//   actions: {
 
-  }
-})
+//   }
+// })
 
-export default store
+// export default store
